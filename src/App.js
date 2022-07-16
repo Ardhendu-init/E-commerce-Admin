f330fc74ffc620dee86040155e8ba201a65274a3
Ardhendu Pramanik <ardhendujj002@gmail.com>
2022-07-16 21:09:28 +0530

    added sidebar , topbar , home page some functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Topbar from "./components/topbar/Topbar";
-
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./Pages/home/Home";
+import "./App.css";
 const App = () => {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+      </div>
     </div>
   );
 };
